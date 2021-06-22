52217e91f82b6ec7677b9d7a9d2e5b142f842cc3
xuxiaokui <xuxiaokui@meprint.com>
2021-06-22 14:40:26 +0800

    修改部署脚本

--- restart.sh
+++ restart.sh
@@ -1,8 +1,24 @@
 #!/bin/sh
 
-cd /home/dev/.jenkins/workspace/autoTest_pipeline_Test
-echo "开始打包编译代码"
-mvn clean install -Dmaven.test.skip=true
-echo "开始重启服务"/home/dev/.jenkins/workspace/autoTest_pipeline_Test
-nohup java -jar /home/dev/.jenkins/workspace/autoTest_pipeline_Test/meprint-system/target/meprint-system-2.6.jar > logs/meprint.log 2>&1 &
+# 变量参数化
+APP_NS="test"
+APP_NAME="qa_test_platform"
+APP_IMAGE="registry-vpc.cn-hangzhou.aliyuncs.com/muri/$APP_NAME:`date +%Y%m%d%H%M%S`"
+
+
+# 打包-->做镜像-->上传镜像仓库
+mvn clean package -U -Dmaven.test.skip=true
+docker build -t $APP_IMAGE -f test_dockerfile .
+docker push $APP_IMAGE
+
+
+# 判断是否为第一次部署
+if [ !-n `kubectl get pods -n $APP_NS |grep "^$APP_NAME"` ];then
+  # 第一次部署
+  sh /usr/local/sbin/k8s.sh $APP_NS $APP_NAME $APP_IMAGE
+  kubectl create -f $PWD/$APP_NAME.yaml
+else
+  echo "开始执行更新操作。。。"
+  kubectl -n $APP_NS set image deployment/$APP_NAME $APP_NAME=$APP_IMAGE
+fi
 echo "启动完成！！！"
