593496ab944738cb4ea4ab6fa532390f4b1def96
xuxiaokui <xuxiaokui@meprint.com>
2021-06-22 14:08:02 +0800

    修改服务启动日志地址

--- restart.sh
+++ restart.sh
@@ -4,5 +4,5 @@ cd /home/dev/.jenkins/workspace/autoTest_pipeline_Test
 echo "开始打包编译代码"
 mvn clean install -Dmaven.test.skip=true
 echo "开始重启服务"/home/dev/.jenkins/workspace/autoTest_pipeline_Test
-nohup java -jar /home/dev/.jenkins/workspace/autoTest_pipeline_Test/meprint-system/target/meprint-system-2.6.jar > /home/dev/.jenkins/workspace/autoTest_pipeline_Test/logs/meprint.log 2>&1 &
+nohup java -jar /home/dev/.jenkins/workspace/autoTest_pipeline_Test/meprint-system/target/meprint-system-2.6.jar > logs/meprint.log 2>&1 &
 echo "启动完成！！！"
