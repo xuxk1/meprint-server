1ee2ca160b9b3f47bc653a8fdd4ad0d5afed1761
xuxiaokui <xuxiaokui@meprint.com>
2021-06-22 13:48:00 +0800

    修改启动脚本编译代码路径

--- restart.sh
+++ restart.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-cd /home/dev/workspace/autoTest_pipeline_Test
+cd /home/dev/.jenkins/workspace/autoTest_pipeline_Test
 echo "开始打包编译代码"
 mvn clean install -Dmaven.test.skip=true
-echo "开始重启服务"
-nohup java -jar /home/dev/workspace/autoTest_pipeline_Test/meprint-system/target/meprint-system-2.6.jar > /home/dev/logs/meprint.log 2>&1 &
+echo "开始重启服务"/home/dev/.jenkins/workspace/autoTest_pipeline_Test
+nohup java -jar /home/dev/.jenkins/workspace/autoTest_pipeline_Test/meprint-system/target/meprint-system-2.6.jar > /home/dev/.jenkins/workspace/autoTest_pipeline_Test/logs/meprint.log 2>&1 &
 echo "启动完成！！！"
